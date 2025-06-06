8b5a9b3b2699ad6c5c4d29ea9f9365017001c450
Valerii Kashpur <67324553+valerii-kashpur@users.noreply.github.com>
2025-06-06 15:17:08 +0300

    phone number fix

--- scrapers/phone_number.py
+++ scrapers/phone_number.py
@@ -1,16 +1,20 @@
+import time
+
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 from utils.logger import logger
 
 
 def scrape_phone_number(driver, url):
     try:
-        show_phone_button = driver.find_element(By.CSS_SELECTOR, ".show-phone-button")
+        show_phone_button = driver.find_element(By.CSS_SELECTOR, "span.phone.bold")
+        print("before click: ", show_phone_button)
         show_phone_button.click()
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".phone")))
-        return driver.find_element(By.CSS_SELECTOR, ".phone").text
+        print("after click: ", show_phone_button)
+        print("tel: ", driver.find_element(By.CSS_SELECTOR, "span.phone.bold").text)
+        time.sleep(0.1)
+        # WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "dev.popup-show-phone")))
+        return driver.find_element(By.CSS_SELECTOR, "span.phone.bold").text
     except:
         logger.warning(f"Phone number not found for {url}")
         return None
